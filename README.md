a4a7e32a37f2a025a5dfae756b22b06c88fe2534
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-09-13 18:49:20 +0200

    First prototype nearly working.

---
+++ README.md
@@ -0,0 +1,49 @@
+Web
+===
+
+# Presentation
+
+# Installation
+
+## Installation of Bower to manage web libraries
+
+    $ sudo npm install -g bower
+
+By supposing that you already have installed [npm](https://www.npmjs.com)
+(generally with Node.js).
+
+## Installation of the Android SDK
+
+See information [here](https://developer.android.com/sdk) to install the Android
+SDK. Then export the variable ``ANDROID_HOME``:
+
+    $ export ANDROID_HOME=~/.android-sdk-linux/
+
+You can add this line in your *.bashrc* file.
+
+## Installation of PhoneGap and Cordova
+
+    $ npm install -g phonegap
+    $ npm install -g cordova
+
+## Compilation of the application for Android
+
+    $ git clone
+    $ cd client/
+    $ bower install
+    $ cordova plugin add cordova-plugin-whitelist
+    $ cordova plugin add cordova-plugin-dialogs
+    $ cordova plugin add org.apache.cordova.network-information
+    $ cordova platform add android
+    $ cordova build android
+    $ adb install -r platforms/android/build/outputs/apk/android-debug.apk
+
+## Compilation of the application for iPhone
+
+# License
+
+[GNU Affero General Public License version 3](https://www.gnu.org/licenses/agpl-3.0.html).
+
+# Contact
+
+[Cédric Bonhomme](https://www.cedricbonhomme.org).
---
+++ www/js/app.js
@@ -0,0 +1,81 @@
+// Define application object and common functions.
+var app = (function()
+{
+    // Application object.
+    var app = {};
+
+	app.v2_API = "https://pyaggr3g470r.herokuapp.com/api/v2.0/";
+
+    // ------------- Private helper function ------------- //
+
+    function onDeviceReady()
+    {
+        // TODO: Add functionality if needed.
+        //hyper.log('onDeviceReady')
+    }
+
+    // ------------- Application functions ------------- //
+    app.date_to_hour = function(date) {
+        return new Date(date.replace(' ', 'T')).toLocaleString('en-US', { hour12: false });
+    }
+
+    app.load = function(service, load_objects, data) {
+        $.ajax({
+            type: 'GET',
+            url: app.v2_API + service,
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(data),
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " +
+                        btoa(window.localStorage.getItem("nickname")
+                            + ":" +
+                            window.localStorage.getItem("password")));
+            },
+            success: function (objects) {
+                load_objects(objects);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown){
+                console.log(errorThrown);
+                if (errorThrown == "UNAUTHORIZED") {
+                    window.location.replace("signin.html");
+                }
+            }
+        });
+    }
+
+    app.showScreen = function(screenId)
+    {
+        // Hide current screen if set.
+        if (app.currentScreenId != null)
+        {
+            $('#' + app.currentScreenId).hide();
+        }
+
+        // Show new screen.
+        app.currentScreenId = screenId;
+        $('#' + app.currentScreenId).show();
+        document.body.scrollTop = 0;
+    };
+
+    app.showHomeScreen = function()
+    {
+        app.showScreen('id-screen-home');
+    };
+
+    app.onNavigateBack = function()
+    {
+        history.back();
+    };
+
+    // ------------- Initialisation ------------- //
+
+    document.addEventListener('deviceready', onDeviceReady, false);
+
+    app.showHomeScreen();
+
+    // ------------- Return application object ------------- //
+
+    return app;
+
+})();
